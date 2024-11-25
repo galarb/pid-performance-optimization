138a391ccbc606c860d69205ffb6acbb988fc67e
gal arbel <84223483+galarb@users.noreply.github.com>
2024-11-25 16:13:25 +0200

    noSD

--- robot.py
+++ robot.py
@@ -45,16 +45,17 @@ try:
     sd = sdcard.SDCard(spi, sd_cs)
     os.mount(sd, '/sd')
     print("Files on SD card:", os.listdir('/sd'))
-    read_bmp_in_chunks('/sd/gal.bmp', display, x_offset=85, y_offset=260)
-
+    #read_bmp_in_chunks('/sd/gal.bmp', display, x_offset=85, y_offset=260)
+    
 except OSError as e:
     print("Error initializing SD card:", e)
     
-robot = motordriver.motordriver(encoder1_pin, encoder2_pin, in1_pin, in2_pin, wheel_size, display, arcade)
+robot = motordriver.motordriver(encoder1_pin, encoder2_pin, in1_pin, in2_pin, wheel_size, display, espresso_dolce)
 robot.reset_iterations()
 #print('displacement = ', robot.gomm(100, 240*3)) #distance, number of iterations
-robot.godegreesp(360, 3*240, 2, 0, 0, color565(0, 255, 0)) # distance, number of iterations, Kp, Ki, Kd
-robot.godegreesp(720, 3*240, 5, 1, 0, color565(255, 0, 0)) # distance, number of iterations, Kp, Ki, Kd
-robot.godegreesp(1040, 3*240, 2, 3, 1, color565(255, 80, 255))
+robot.godegreesp(360, 240, 1.5, 0, 0, color565(255, 0, 0), line_index=0)
+robot.godegreesp(720, 240, 1.5, 1, 0, color565(220, 60, 220), line_index=1)
+robot.godegreesp(1060, 240, 1.5, 0.5, 0.1, color565(0, 0, 255), line_index=2)
+robot.godegreesp(1440, 240, 1.5, 0.7, 0.2, color565(255, 255, 0), line_index=3)
 robot.motgo(0)
 # try: godegrees(angle, times) and godegreesp(angle, times, kp, ki, kd)
